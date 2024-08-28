1b95dea831ff79c3c5535713d87e384276d6b5e0
AhminaMar1 <AhminaMar1@gmail.com>
2024-08-28 20:54:26 +0100

    fix(): remove experimental specifier sesolution

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import { resourcesPath, fileServerPort } from './src/config';
-import { startFileServer } from './src/fileServer';
-
+import { resourcesPath, fileServerPort, proxyServerPort } from './src/config.js';
+import { startFileServer } from './src/fileServer/index.js';
+import { startProxy } from './src/proxyServer/index.js';
 
 // to understand the structure => look at README.md
 
