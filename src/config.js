5261445464d41e2758c540b37e4d061bf2037143
AhminaMar1 <AhminaMar1@gmail.com>
2024-08-29 21:51:59 +0100

    feat(): implement subdirResolver helpers

--- src/config.js
+++ src/config.js
@@ -6,3 +6,7 @@ export const proxyServerPort = 1314;
 
 // resources path that will be servert by the fileServer.
 export const resourcesPath = './resourcesExample';
+
+//
+export const EXCLUDED_EXT = new Set(['svg', 'png']);
+export const EXCLUDE_LEGACY = 'legacy';
