ab524ca3d66e0d6c6c3c6e65b695acc5487b9c39
AhminaMar1 <AhminaMar1@gmail.com>
2024-09-02 23:25:00 +0100

    feat(): resolve path dirs slashes

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 // files
 
+export const DEEP_PATH_INTO_CONSIDERATION = 2;
+
 // Ports
 export const fileServerPort = 1313;
 export const proxyServerPort = 1314;
 
 // resources path that will be servert by the fileServer.
-export const resourcesPath = './resourcesExample';
+export const ROOT_PATH = './resourcesExample';
 
 //
 export const EXCLUDED_EXT = new Set(['svg', 'png']);
