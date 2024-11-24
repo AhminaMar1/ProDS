f39d3195b9608213510391176728d457bb154464
AhminaMar1 <AhminaMar1@gmail.com>
2024-11-24 16:12:48 +0100

    feat(): use dynamic deplth and modifiy ports

--- config.js
+++ config.js
@@ -5,12 +5,9 @@ export const BASE_DOMAIN_URL = 'https://example.com';
 // enable flows
 export const ENABLED_FLOWS = ['app01', 'app02'];
 
-// files config
-export const DEEP_PATH_INTO_CONSIDERATION = 2;
-
 // Ports
-export const FILES_SERVER_PORT = 1313;
-export const PROXY_SERVER_PORT = 1314;
+export const PROXY_SERVER_PORT = 1313;
+export const FILES_SERVER_PORT = 1314;
 
 //URLs
 export const LOCALHOST = 'http://127.0.0.1';
@@ -22,7 +19,7 @@ export const ROOT_PATH = './resources';
 export const ROOT_PATH_LEN =
 	ROOT_PATH.charAt(0) === '.' ? ROOT_PATH.length : ROOT_PATH.length + 2;
 
-//
+//INCLUDEDS/EXCLUDEDS
 export const INCLUDED_EXT = new Set(['js', 'css']);
 export const EXCLUDED_EXT = new Set(['svg', 'png']);
 export const HAS_TO_EXCLUDE_LEGACY = true;
--- src/proxyServer/proxyServer.js
+++ src/proxyServer/proxyServer.js
@@ -2,10 +2,15 @@ import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
 import chalk from 'chalk';
-import {LOCALHOST, URL_FILES_SERVER, EXCLUDE_WITH_NO_PRODS} from '../../config.js';
+import {
+	LOCALHOST,
+	URL_FILES_SERVER,
+	INCLUDED_EXT,
+	EXCLUDE_WITH_NO_PRODS,
+} from '../../config.js';
 import {hashFn} from '../helpers/hashFn.js';
 import {excludedfullUrl} from '../helpers/urlResolver.js';
-import {trieMatch, resolve_url} from '../helpers/proDSTrie.js';
+import {trieMatch} from '../helpers/proDSTrie.js';
 import {splitNameDirFile} from '../helpers/splitNameDirFile.js';
 
 const app = express();
@@ -15,23 +20,23 @@ http.globalAgent.keepAlive = true;
 const startProxy = ({PORT, hashObj, hashSet}) => {
 	app.use('*', async function (req, res) {
 		const {baseUrl} = req;
+		const toGetExt = baseUrl.split('.');
+		const hasIncludedExt = INCLUDED_EXT.has(toGetExt.pop() || '');
 		console.log(chalk.blue('In: ', baseUrl));
-		if (!baseUrl.includes(EXCLUDE_WITH_NO_PRODS)) {
+		if (hasIncludedExt) {
 			const keyFileName = hashFn(baseUrl);
 			if (keyFileName && hashSet.has(keyFileName)) {
 				const redirectTo = URL_FILES_SERVER + hashObj[keyFileName];
 				const fileDirAndName = hashObj[keyFileName];
 				console.log(chalk.green('Out: file found, file link:'), chalk.bgGreen(redirectTo));
 				const [fileName, fileDir] = splitNameDirFile(fileDirAndName);
-				console.log(fileDir + fileName);
 				res.sendFile(fileName, {root: fileDir}, (err) => {
 					if (err) console.log(chalk.red('Error when send file:'), chalk.bgRed(err));
 				});
 			} else {
 				const tryTrie = trieMatch(baseUrl);
 				if (tryTrie && (tryTrie.success || tryTrie.resolved)) {
-					const rFile = tryTrie.res;
-					const rUrl = resolve_url(baseUrl, rFile);
+					const rUrl = tryTrie.res;
 					const redirectTo = URL_FILES_SERVER + rUrl;
 					const fileDirAndName = rUrl;
 					if (tryTrie.success) {
@@ -62,13 +67,15 @@ const startProxy = ({PORT, hashObj, hashSet}) => {
 				}
 			}
 		} else {
+			const url = excludedfullUrl(baseUrl);
+			const alreadyHasExluded = baseUrl.split('#').pop() === EXCLUDE_WITH_NO_PRODS;
 			console.log(
-				chalk.red(
-					'Out: the url that contain ',
-					EXCLUDE_WITH_NO_PRODS,
-					' not has to redirect to the ProDS solution',
-				),
+				chalk.red('Out: the file extension not allowed, so redirect into'),
+				chalk.bgRed(url),
 			);
+			if (!alreadyHasExluded) {
+				res.redirect(baseUrl);
+			}
 		}
 	});
 
